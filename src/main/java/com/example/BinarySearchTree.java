e4b397063685a2f7b76d2145873a4b261c83470f
Solbon from Kizhinga <solbon@gmail.com>
2017-05-28 21:44:32 +0800

    Empty project

---
+++ src/main/java/com/example/BinarySearchTree.java
@@ -0,0 +1,7 @@
+package com.example;
+
+/**
+ * Created by Гунга on 28.05.2017.
+ */
+public class BinarySearchTree {
+}
---
+++ src/main/java/com/example/Tree.java
@@ -0,0 +1,9 @@
+package com.example;
+
+/**
+ * Created by Гунга on 28.05.2017.
+ */
+public interface Tree {
+    public boolean isEmpty();
+    
+}
